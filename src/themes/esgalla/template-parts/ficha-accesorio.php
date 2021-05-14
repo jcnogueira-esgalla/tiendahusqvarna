0d65fc899df36d5f59dea111e2f3b46e103913af
Francisco Ruiz <francisco.ruiz@esgalla.com>
2021-05-14 12:35:36 +0200

    Traducir Añadir a tienda

--- src/themes/esgalla/template-parts/ficha-accesorio.php
+++ src/themes/esgalla/template-parts/ficha-accesorio.php
@@ -24,9 +24,9 @@ $producto = wc_get_product( $id_producto );
 		</div>
 
 		<?php if($producto->get_stock_status()=='instock' && !in_array(wp_get_post_terms( $producto->id, 'product_cat' )[0]->term_id, [1815, 479])): ?>
-			<button href="<?php echo get_site_url().'/?add-to-cart='.$producto->get_id() ?>" class="btn btn-primary btn-cart text-white spacing font-weight-regular border-0 fs-125 text-center text-uppercase boton-add-cart-ficha" data-quantity="1" data-product_id="<?php echo $producto->get_id() ?>"><?php _e('AÑADIR AL CARRITO') ?></button>
+			<button href="<?php echo get_site_url().'/?add-to-cart='.$producto->get_id() ?>" class="btn btn-primary btn-cart text-white spacing font-weight-regular border-0 fs-125 text-center text-uppercase boton-add-cart-ficha" data-quantity="1" data-product_id="<?php echo $producto->get_id() ?>"><?php _e('AÑADIR AL CARRITO', 'esgalla') ?></button>
 		<?php else: ?>
-			<button href="<?php echo get_permalink( $producto->get_id() ) ?>" class="btn btn-primary btn-cart text-white spacing font-weight-regular border-0 fs-125 text-center text-uppercase" ><?php _e('VER PRODUCTO') ?></button>
+			<button href="<?php echo get_permalink( $producto->get_id() ) ?>" class="btn btn-primary btn-cart text-white spacing font-weight-regular border-0 fs-125 text-center text-uppercase" ><?php _e('VER PRODUCTO', 'esgalla') ?></button>
 		<?php endif; ?>
 		<?php /* <button href="#" class="btn btn-primary btn-cart text-white spacing font-weight-regular border-0 fs-125 text-center text-uppercase mt-2">Añadir a la cesta</button> */ ?>
 	</a>
