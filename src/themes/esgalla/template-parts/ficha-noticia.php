1515db24713d690ff93adc2719f06f0bfffed714
Francisco Ruiz <francisco.ruiz@esgalla.com>
2021-05-14 11:34:22 +0200

    Edit Blog traduccion

--- src/themes/esgalla/template-parts/ficha-noticia.php
+++ src/themes/esgalla/template-parts/ficha-noticia.php
@@ -2,6 +2,6 @@
 <a href="<?php echo get_the_permalink($id_noticia); ?>" class="ficha-noticia d-flex flex-column justify-content-between text-decoration-none shadow-sm rounded bg-white mb-3">
 	<h2 class="h6 fs-18 font-weight-bold text-dark"><?php echo get_the_title($id_noticia); ?></h2>
 	<div class="text-primary link-noticia pt-3 font-weight-bold font-italic fs-18 position-relative">
-		leer noticia
+	<?php _e("leer noticia","esgalla"); ?>
 	</div>
 </a>
